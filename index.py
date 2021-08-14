6367c0498804cb4daf10e1bb1d7c64d9ef17b1f6
juicyjung <travelandi01@gmail.com>
2021-08-15 04:29:19 +0900

    dialog complete

--- index.py
+++ index.py
@@ -9,24 +9,67 @@ class MyWindow(QMainWindow):
 
         self.update_button.clicked.connect(self.update)
 
+        self.update_goals()
+        self.update_table()
+
         self.show()
 
     def update(self):
-        self.widget= MyApp()
-        self.widget.show()
+        dlg = MyApp()
+        dlg.exec_()
+
+        self.update_goals()
+        self.update_table()
+
+        # self.widget= MyApp()
+        # self.widget.show()
+
+    def update_goals(self):
+        with open("goals.txt") as f:
+            self.goal.setText("$" + f.readline())
+
+    def update_table(self):
+        with open("table.txt") as f:
+            lines = f.readlines()
+            for i in range(len(lines)):
+                self.table.setItem(int(i/2)+1, (i % 2) +1, QTableWidgetItem(lines[i].strip()))
 
 
-
-
-class MyApp(QWidget):
+class MyApp(QDialog):
     def __init__(self):
         super().__init__()
-        uic.loadUi("change.ui", self)
+        uic.loadUi("update.ui", self)
 
-        self.update_button.clicked.connect(self.update)
-    
-    def update(self):
-        print(self.goals.text())
+        self.update_button.clicked.connect(self.pushButtonClicked)
+
+        with open("goals.txt") as f:
+            line = f.readline()
+            if line == "":
+                line = "Enter your goals"
+
+        self.goals.setPlaceholderText(line)
+
+        with open("table.txt") as f:
+            lines = f.readlines()
+
+            for i in range(len(lines)):
+                self.table.setItem(int(i/2), i % 2, QTableWidgetItem(lines[i].strip()))
+
+
+    def pushButtonClicked(self):
+        if self.goals.text() != "":
+            with open("goals.txt", "w") as f:
+                f.write(self.goals.text())
+
+        with open("table.txt", "w") as f:
+            for i in range(20):
+                if self.table.item(int(i/2), i % 2) != None:
+                    f.write(f"{self.table.item(int(i/2), i % 2).text()}\n")
+
+        for i in range(20):
+            if self.table.item(int(i/2), i % 2) != None:
+                print(self.table.item(int(i/2), i % 2).text())
+
         self.close()
 
 
