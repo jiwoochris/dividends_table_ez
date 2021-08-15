09948362d0175d5fa122e04ed9d1779b305a124a
juicyjung <travelandi01@gmail.com>
2021-08-15 21:36:50 +0900

    almost done

--- 3-4-1.py
+++ 3-4-1.py
@@ -1,18 +1,39 @@
 # from pandas_datareader import data as pdr
 import yfinance as yf
+import datetime
+import pandas as pd
 # yf.pdr_override()
 import pprint
 
-msft = yf.Ticker("PEP")
+msft = yf.Ticker("APPL")
 
 # pprint.pprint(msft.info)
+print(msft.info['longName'])
 
+print(msft.info)
+
+if msft.info['sector'] == None:
+    pprint.pprint(msft.info)
 print(msft.info['sector'])
 print(msft.info['industry'])
-print(msft.info['longName'])
+
 print(msft.info['currentPrice'])
 
 
 print(msft.dividends[-12:].index)
 
-s.unique()
+s = pd.Series(msft.dividends[-12:].index)
+print(s.dt.month.unique())
+
+# print(str(msft.dividends[-1]))
+
+print(msft.dividends[-1])
+
+print(pd.Series(msft.dividends[-12:].index).dt.month.nunique())
+
+print(pd.Series(msft.dividends[-4:].index).dt.month.unique())
+
+print(set(pd.Series(msft.dividends[-12:].index).dt.month))
+
+
+# s.unique()
--- index.py
+++ index.py
@@ -9,8 +9,7 @@ class MyWindow(QMainWindow):
 
         self.update_button.clicked.connect(self.update)
 
-        self.update_goals()
-        self.update_table()
+        self.upload()
 
         self.show()
 
@@ -18,21 +17,69 @@ class MyWindow(QMainWindow):
         dlg = MyApp()
         dlg.exec_()
 
-        self.update_goals()
-        self.update_table()
+        self.upload()
 
         # self.widget= MyApp()
         # self.widget.show()
 
-    def update_goals(self):
-        with open("goals.txt") as f:
-            self.goal.setText("$" + f.readline())
+    def upload(self):
+        balance = 0
+        div = 0
+        cal = ["", "", "", "", "", "", "", "", "", "", "", ""]
+        div_cal = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        
+        for i in range(7):
+            for j in range(9):
+                self.table.setItem(i+1, j+1, None)
 
-    def update_table(self):
-        with open("table.txt") as f:
+        # self.table.clear()
+        # self.table.setItem(0, 0, QTableWidgetItem("long name"))
+
+        with open("goals.txt") as f:
+            goal = f.readline()
+            self.goal.setText("$" + goal)
+
+        with open("show_table.txt") as f:
             lines = f.readlines()
             for i in range(len(lines)):
-                self.table.setItem(int(i/2)+1, (i % 2) +1, QTableWidgetItem(lines[i].strip()))
+                self.table.setItem(int(i/7)+1, (i % 7), QTableWidgetItem(lines[i].strip()))
+                if i % 7 == 3:
+                    balance += float(lines[i].strip())
+                if i % 7 == 5:
+                    div = div + float(lines[i].strip())
+                if i % 7 == 6:
+                    if lines[i].strip() == "monthly":
+                        for j in range(12):
+                            if cal[j] == "":
+                                cal[j] = lines[i-5].strip()
+                            else:
+                                cal[j] = cal[j] + ", " + lines[i-5].strip()
+                            div_cal[j] += float(lines[i-2].strip())
+
+
+                    for j in range(12):
+                        if str(j+1) in lines[i].strip()[1:-1].split(", "):
+                            if cal[j] == "":
+                                cal[j] = lines[i-5].strip()
+                                div_cal[j] += float(lines[i-2].strip())
+                            else:
+                                cal[j] = cal[j] + ", " + lines[i-5].strip()
+                                div_cal[j] += float(lines[i-2].strip())
+                
+            print(cal)
+
+            for i in range(12):
+                self.calendar.setItem(1, i, QTableWidgetItem(cal[i]))
+                self.calendar.setItem(2, i, QTableWidgetItem(f"{div_cal[i] : .3f}"))
+            
+            self.balance.setText(f"$ {balance : .3f}")
+            self.div.setText(f"$ {div : .3f} / $ {div/12 : .3f}")
+
+            self.progressBar.setValue(int(div/12 / float(goal.strip()) * 100))
+
+            self.calendar.setRowHeight(1, 80)
+            
+
 
 
 class MyApp(QDialog):
@@ -57,6 +104,9 @@ class MyApp(QDialog):
 
 
     def pushButtonClicked(self):
+        import yfinance as yf
+        import pandas as pd
+
         if self.goals.text() != "":
             with open("goals.txt", "w") as f:
                 f.write(self.goals.text())
@@ -66,9 +116,30 @@ class MyApp(QDialog):
                 if self.table.item(int(i/2), i % 2) != None:
                     f.write(f"{self.table.item(int(i/2), i % 2).text()}\n")
 
-        for i in range(20):
-            if self.table.item(int(i/2), i % 2) != None:
-                print(self.table.item(int(i/2), i % 2).text())
+        with open("show_table.txt", "w") as f:
+            for i in range(20):
+                if self.table.item(int(i/2), i % 2) != None:
+                    if self.table.item(int(i/2), i % 2).text() != "":
+                        print(self.table.item(int(i/2), i % 2).text())
+                        if i % 2 == 0:
+                            msft = yf.Ticker(self.table.item(int(i/2), 0).text())
+                            f.write(f"{msft.info['longName']}\n")
+                            f.write(f"{msft.info['symbol']}\n")
+
+                        if i % 2 == 1:
+                            amount = int(self.table.item(int(i/2), i % 2).text())
+                            f.write(f"{amount}\n")
+                            f.write(f"{msft.info['currentPrice'] * amount : .3f}\n")
+                            f.write(f"{amount * msft.dividends[-1] * 0.85 : .3f}\n")
+
+                            if pd.Series(msft.dividends[-12:].index).dt.month.nunique() == 12:
+                                f.write(f"{12* amount * msft.dividends[-1] : .3f}\n")
+                                f.write(f"monthly\n")
+                            else:
+                                f.write(f"{pd.Series(msft.dividends[-4:].index).dt.month.nunique() * amount * msft.dividends[-1] * 0.85 : .3f}\n")
+                                f.write(f"{set(pd.Series(msft.dividends[-4:].index).dt.month)}\n")
+                            
+                    
 
         self.close()
 
